a32b4c847b3332efcc9b8c06672da4af894a33d5
Davis Keene <daviskeene@Daviss-MacBook-Pro.local>
2022-01-21 12:43:13 -0600

    add: project files

---
+++ main.py
@@ -0,0 +1,98 @@
+import os
+import datetime
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+from dotenv import load_dotenv
+from flask import Flask
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+import sheets as sh
+
+load_dotenv()
+
+app = Flask(__name__)
+
+def send_email():
+    sender_email = "uiuc.schooldigest@gmail.com"
+    receiver_email = "dbkeene.tsyc@gmail.com"
+    password = os.getenv("GMAIL_PWD")
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = f"{datetime.datetime.now().strftime('%m/%d/%y')} UIUC Digest"
+    message["From"] = sender_email
+    message["To"] = receiver_email
+
+    def get_bulk_records():
+        client = sh.get_gsheet_client(os.getenv("PATH_TO_CLIENT_SECRET"))
+        sheet = sh.get_google_sheet(client)
+
+        assignments = sh.get_records(sheet, "Assignment")
+        quizzes = sh.get_records(sheet, "Quiz")
+        exams = sh.get_records(sheet, "Exam")
+        reminders = sh.get_records(sheet, "Reminder")
+
+        return [assignments, quizzes, exams, reminders]
+
+
+    def format_records(record: list):
+        html_str = ""
+        for r in record:
+            html_str += f"""
+                <b> {r['Course Code']} </b> - {r['Name']} ({r['Due Date']})<br>
+            """
+        return html_str
+
+    records = get_bulk_records()
+    # Create the plain-text and HTML version of your message
+    html = f"""\
+    <html>
+    <body>
+        <p>Hi <b>Davis</b>,<br>
+        <p>Here's your email digest for the week!<p>
+
+            <h2 style="font-weight: bold;">
+                Assignments
+            </h2>
+
+            {format_records(records[0]) or "No assignments due this week."}
+
+            <h2 style="font-weight: bold;">
+                Quizzes / Exams
+            </h2>
+
+            {format_records(records[1]) or "No quizzes for this week."}
+            <br>
+            {format_records(records[2]) or "No exams for this week."}
+
+            <h2 style="font-weight: bold;">
+                Reminders
+            </h2>
+
+            {format_records(records[3]) or "No reminders for this week."}
+        </p>
+    </body>
+    </html>
+    """
+
+    print(html)
+
+    # Turn these into plain/html MIMEText objects
+    html = MIMEText(html, "html")
+
+    # Add HTML/plain-text parts to MIMEMultipart message
+    # The email client will try to render the last part first
+    message.attach(html)
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
+
+if __name__ == '__main__':
+    scheduler = BackgroundScheduler()
+    job = scheduler.add_job(send_email, 'cron', day_of_week='mon-sun', hour=9, minute=0)
+    scheduler.start()
+    app.run(host="0.0.0.0")
---
+++ sheets.py
@@ -0,0 +1,94 @@
+from datetime import date, datetime, timedelta
+import os
+from typing import List
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# # # # # # # # # #
+#                 #
+#   Abstractions  #
+#                 #
+# # # # # # # # # #
+
+
+def get_gsheet_client(path_to_client_secret: str) -> gspread.Client:
+    # Get Google Sheet client using scope + service account credentials
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/drive",
+    ]
+    creds = ServiceAccountCredentials.from_json_keyfile_name(
+        path_to_client_secret, scope
+    )
+    return gspread.authorize(creds)
+
+
+def get_google_sheet(client: gspread.Client):
+    # Returns the discord membership spreadsheet.
+    return client.open("Semester Data Table").sheet1
+
+
+def write_to_cell(sheet, row: int, col: int, value: str) -> None:
+    # Updates a particular cell in the sheet.
+    sheet.update_cell(row, col, value)
+
+
+def write_to_row(sheet, value: list, index: int) -> None:
+    # Inserts a row into a particular index in our sheet.
+    sheet.insert_row(value, index)
+
+
+def write_to_next_row(sheet, value: list) -> None:
+    write_to_row(
+        sheet, value, 2
+    )  # Row count doesn't include headers, insert at index 2
+
+
+def append_row(sheet, value: list) -> None:
+    # Appends a row to the sheet
+    sheet.append_row(value)
+
+
+def get_all_formatted_records(sheet):
+    records = sheet.get_all_records()
+    # Convert date if not in correct format
+    for i, record in enumerate(records):
+        try:
+            date = datetime.strptime(x["Due Date"], "%m/%d/%y")
+        except:
+            # On exception, change date
+            records[i]['Due Date'] = '/'.join(record['Due Date'].split('/')[:-1]) + f'/{str(datetime.today().year)[2:]}'
+    # print(records)
+    return records
+
+
+def get_records(sheet, type: str, days: int = 7):
+    # Get record of type 'type' due within a certain amount of days (default = 7).
+    return [
+        x
+        for x in get_all_formatted_records(sheet)
+        if x["Type"] == type
+        and datetime.strptime(x["Due Date"], "%m/%d/%y")
+        < datetime.now() + timedelta(days=days)
+    ]
+
+
+def main():
+    # Find a workbook by name and open the first sheet
+    # Make sure you use the right name here.
+    client = get_gsheet_client(os.getenv("PATH_TO_CLIENT_SECRET"))
+    sheet = get_google_sheet(client)
+
+    # Extract and print all of the values
+    print(get_records(sheet, "Assignment"))
+
+
+if __name__ == "__main__":
+    # Running this file as a script will validate our connection
+    # and print the number of users registered with Discord.
+    main()
